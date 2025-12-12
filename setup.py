57b297a437d4f9c16c0f695ef4c4228919b9a138
kolikaran1992 <kolikaran1992@gmail.com>
2025-12-12 10:11:41 +0530

    revert to setuptools for builds

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 
 extensions = cythonize(
@@ -11,4 +11,11 @@ extensions = cythonize(
     compiler_directives={"language_level": "3"},
 )
 
-setup(ext_modules=extensions)
+setup(
+    name="voice-forge",
+    version="0.1.0",
+    packages=find_packages(include=["voice_forge", "voice_forge.*"]),
+    ext_modules=extensions,
+    include_package_data=True,
+    package_data={"voice_forge": ["settings_file/*.toml"]},
+)
